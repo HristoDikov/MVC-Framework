f64d06cd20af2e1a5673c2262b361258ae373517
Hristo Dikov <dikoww7@gmail.com>
2022-02-21 20:16:14 +0200

    Add override ToString to Reponse Cookie. Change MaxAge to nullable.

--- HIS/HIS.Http/ResponseCookie.cs
+++ HIS/HIS.Http/ResponseCookie.cs
@@ -1,12 +1,13 @@
 ﻿namespace HIS.Http
 {
     using System;
+    using System.Text;
 
     public class ResponseCookie : Cookie
     {
 
         public ResponseCookie(string name, string value)
-            :base(name, value)
+            : base(name, value)
         {
             this.Path = "/";
             this.SameSite = SameSiteType.None;
@@ -20,12 +21,53 @@
 
         public DateTime? Expires { get; set; }
 
-        public long MaxAge { get; set; }
+        public long? MaxAge { get; set; }
 
         public bool Secure { get; set; }
 
         public bool HttpOnly { get; set; }
 
         public SameSiteType SameSite { get; set; }
+
+        public override string ToString()
+        {
+            StringBuilder cookieBuilder = new StringBuilder();
+
+            cookieBuilder.Append($"{this.Name}={this.Value}");
+
+            if (this.MaxAge.HasValue)
+            {
+                cookieBuilder.Append($"; Max-Age=" + this.MaxAge.Value.ToString());
+
+            }
+            else if (this.Expires.HasValue)
+            {
+                cookieBuilder.Append($"; Expires=" + this.Expires.Value.ToString("R"));
+            }
+
+            if (!string.IsNullOrWhiteSpace(this.Domain))
+            {
+                cookieBuilder.Append($"; Domain=" + this.Domain);
+            }
+
+            if (!string.IsNullOrWhiteSpace(this.Path))
+            {
+                cookieBuilder.Append($"; Path=" + this.Path);
+            }
+
+            if (this.Secure == true)
+            {
+                cookieBuilder.Append($"; Secure=");
+            }
+
+            if (this.HttpOnly == true)
+            {
+                cookieBuilder.Append($"; HttpOnly=");
+            }
+
+            cookieBuilder.Append("; SameSite=" + this.SameSite.ToString());
+
+            return cookieBuilder.ToString();
+        }
     }
 }
